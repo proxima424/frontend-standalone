76d8931fe6c7a66b5a79473144d2ee85b5aad7c4
Shivannsh <bb1221392@iitd.ac.in>
2024-12-25 22:50:39 +0530

    Landing page updated

--- pnp/src/App.js
+++ pnp/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import Spline from '@splinetool/react-spline';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { motion } from 'framer-motion';
@@ -8,19 +9,26 @@ import TokenMarket from './pages/TokenMarket';
 
 function HomePage() {
   return (
-    <div className="main-container">
-      <div className="content">
-        <div className="title-container">
-          <motion.h1 className="title" initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>Permissionless</motion.h1>
-          <motion.h1 className="title poppins-font" initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.2 }}>Prediction</motion.h1>
-          <motion.h1 className="title poppins-font" initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.4 }}>Markets</motion.h1>
-          <motion.h2 className="subheading" initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.6 }}>Create auto-settling prediction markets without any third party approval or fees or optimistic faith in the oracle.</motion.h2>
-        </div>
-        <div className="spline-container">
-          <Spline scene="https://prod.spline.design/mLlfM-mUgJQCxt1F/scene.splinecode" />
+    <>
+      <div className="main-container">
+        <div className="content">
+          <div className="title-container">
+            <motion.h1 className="title" initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>Permissionless</motion.h1>
+            <motion.h1 className="title special-head poppins-font" initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.2 }}>Prediction</motion.h1>
+            <motion.h1 className="title poppins-font" initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.4 }}>Markets</motion.h1>
+            <motion.h2 className="subheading" initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.6 }}>Create auto-settling prediction markets without any third party approval or fees or optimistic faith in the oracle.</motion.h2>
+            <div className="button-container">
+              <a className="btn-primary" href="/trade">Start Trading</a>
+              <a className="btn-secondary" href="/dashboard">View Dashboard</a>
+            </div>
+          </div>
+          <div className="spline-container">
+            <Spline scene="https://prod.spline.design/mLlfM-mUgJQCxt1F/scene.splinecode" />
+          </div>
         </div>
       </div>
-    </div>
+
+    </>
   );
 }
 
